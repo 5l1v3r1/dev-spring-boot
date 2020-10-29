8c46db24d38b5c8ec91d4101434cf853449f4a01
firoz alam <firozalam49@gmail.com>
2020-10-29 16:51:46 +0530

    Coach module firoz 29-10

--- src/main/java/com/mdms/mdms_coach/coachuncleansed/controller/CoachEditForwardController.java
+++ src/main/java/com/mdms/mdms_coach/coachuncleansed/controller/CoachEditForwardController.java
@@ -8,15 +8,19 @@
 
 package com.mdms.mdms_coach.coachuncleansed.controller;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mdms.mdms_coach.coachuncleansed.model.CoachDataCMM;
+import com.mdms.mdms_coach.coachuncleansed.model.CoachUncleansedData;
 import com.mdms.mdms_coach.coachuncleansed.service.CoachEditForwardService;
 
 
@@ -27,21 +31,31 @@ public class CoachEditForwardController {
 	
 	Logger logger=LoggerFactory.getLogger(CoachEditForwardController.class);
 	
+	
+	//-------------------------------------------return coachIds for a particular depot--------------------------------------------
+	
 	@RequestMapping(method=RequestMethod.GET, value="/getCoachesByDepot")
 	public List<String> getCoachesByDepot(@RequestParam String depotId) {	
+		
+		logger.info("Controller : CoachEditForwardController || Method: getCoachesByDepot || getCoachForDepot: "+depotId);
 		List<String> coachIds=coachEditFwdServ.getCoachesByDepot(depotId);
-		logger.info("Controller : CoachEditForwardController || Method: getCoachesByDepot || getCoachForDepot: "+coachIds);
 			return coachIds;	
 	}
 	
+	//-------------------------------------------return coachDetails for a particular coach Id--------------------------------------------
+	@RequestMapping(method=RequestMethod.GET, value="/getCoachByCoachId")
+	public Optional<CoachDataCMM> getCoachByCoachId(@RequestParam String coachId) {	
+		Optional<CoachDataCMM> coachDetails=coachEditFwdServ.getCoachByCoachId(coachId);
+		logger.info("Controller : CoachEditForwardController || Method: getCoachesByDepot || getCoachDetailsforCoachId: "+coachId);
+			return coachDetails;	
+	}
 	
-	
+
+	//-------------------------------------------coach Details forwarded/saved as draft after cleaning of record--------------------------------------------
 	@RequestMapping(method=RequestMethod.POST, value="/editForwardCoach")
-	public boolean EditForwardCoach() {
-		
-		boolean flag=coachEditFwdServ.editForwardCoach();
-		
-			return flag;	
+	public String EditForwardCoach(@RequestBody CoachUncleansedData coachUncleansedData) {		
+		String response=coachEditFwdServ.editForwardCoach(coachUncleansedData);	
+			return response;	
 	}
 	
 }
--- src/main/java/com/mdms/mdms_coach/coachuncleansed/service/CoachEditForwardService.java
+++ src/main/java/com/mdms/mdms_coach/coachuncleansed/service/CoachEditForwardService.java
@@ -1,6 +1,7 @@
 package com.mdms.mdms_coach.coachuncleansed.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,25 +9,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import com.mdms.mdms_coach.coachuncleansed.model.CoachDataCMM;
+import com.mdms.mdms_coach.coachuncleansed.model.CoachUncleansedData;
+import com.mdms.mdms_coach.coachuncleansed.repository.CoachCMMDataRepository;
+
 
 
 @Service
 public class CoachEditForwardService {
 	
+	@Autowired
+	private CoachCMMDataRepository coachEditForwardRepo;
 	Logger logger=LoggerFactory.getLogger(CoachEditForwardService.class);
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-	public boolean editForwardCoach() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	
+	
+//-------------------------------------------return coachIds for a particular depot--------------------------------------------
 	public List<String> getCoachesByDepot(String depotId) {
-		 final String getCoachIdsByDepot = "SELECT coach_id FROM mdms_coach.coach_data_cmm where base_depot= \'"+depotId+"\'";
+		 final String getCoachIdsByDepot = "SELECT coach_id FROM mdms_coach.coach_data_cmm where base_depot=\'"+depotId+"\'";
 		    final List<String> coachIds = jdbcTemplate.queryForList(getCoachIdsByDepot, String.class);
 		return coachIds;
 	}
+
+//-------------------------------------------return coachDetails for a particular coach--------------------------------------------
+
+	public Optional<CoachDataCMM> getCoachByCoachId(String coachId) {
+		
+		Optional<CoachDataCMM> coachDetails=coachEditForwardRepo.findByCoachId(coachId);
+		return coachDetails;
+	}
+	
+	
+//-------------------------------------------coach Details forwarded/saved as draft after cleaning of record--------------------------------------------
+	
+	public String editForwardCoach(CoachUncleansedData coachUncleansedData) {
+		// TODO Auto-generated method stub
+		String response="sff";
+		return response;
+	}
+
 
 }

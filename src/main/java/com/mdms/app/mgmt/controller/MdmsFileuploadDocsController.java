6e91c85d916a4254d5eaf27ff0f3a9b298a604d2
anshul <anshul@cris.org.in>
2020-12-04 18:34:18 +0530

    Anshul 04-12-2020

--- src/main/java/com/mdms/app/mgmt/controller/MdmsFileuploadDocsController.java
+++ src/main/java/com/mdms/app/mgmt/controller/MdmsFileuploadDocsController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import java.io.File;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import com.mdms.app.mgmt.service.MdmsFileuploadDocsService;
+@CrossOrigin(origins = {"http://localhost:4200","http://mdms-ng-dev.s3-website.ap-south-1.amazonaws.com"}, maxAge = 4800, allowCredentials = "false")
 
 @RestController
 @RequestMapping("/mdmdocs")

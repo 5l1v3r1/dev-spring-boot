8798ebac03a4606561983a8517cef47bdbd3a557
rajan <rohanrajan41@gmail.com>
2020-12-09 15:09:43 +0530

    SS-SaveasdraftCMI and UpdateDraftCMI

--- src/main/java/com/mdms/mdms_station/stationuncleansed/controller/StationAddController.java
+++ src/main/java/com/mdms/mdms_station/stationuncleansed/controller/StationAddController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mdms.mdms_station.stationuncleansed.model.StationCleansedData;
 import com.mdms.mdms_station.stationuncleansed.service.StationAddService;
 @CrossOrigin(origins = {"http://localhost:4200","http://mdms-ng-dev.s3-website.ap-south-1.amazonaws.com"}, maxAge = 4800, allowCredentials = "false")
 
@@ -29,6 +30,23 @@ public class StationAddController {
 	return stn_add_serv.checkStncodeExit(station_code);	
 	}
 	
+	@RequestMapping(method=RequestMethod.POST, value="/savecmicleanseddraft")
+	public  String saveCmiCleansedDraft(@RequestBody StationCleansedData stationdraftcmi) throws Exception
+	{	//	System.out.println(stationdraftcmi.getStn_Id().getStation_code());
+//		System.out.println(stationdraftcmi.getStn_Id().getStation_valid_from());
+//		System.out.println(stationdraftcmi.getStn_Id().getStation_valid_upto());
+
+		logger.error("controller : StationAddController || Method : saveCmiCleansedDraft || input recieved stationdraftcmi: "+stationdraftcmi);
+		
+		return stn_add_serv.saveCmiCleansedDraft(stationdraftcmi);
+	}
 	
+	@RequestMapping(method=RequestMethod.POST, value="/updatecmidraft")
+	public  String updateCmiDraft(@RequestBody StationCleansedData stationdraftcmi) throws Exception
+	{
+
+		logger.error("controller : StationAddController || Method : updateCmiDraft || input recieved stationdraftcmi: "+stationdraftcmi.getStn_Id().getStation_code());
+		return stn_add_serv.updateCmiDraft(stationdraftcmi);
+	}
 	
 }
--- src/main/java/com/mdms/mdms_station/stationuncleansed/service/StationAddService.java
+++ src/main/java/com/mdms/mdms_station/stationuncleansed/service/StationAddService.java
@@ -1,8 +1,14 @@
 package com.mdms.mdms_station.stationuncleansed.service;
 
+import java.util.Date;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mdms.mdms_station.stationuncleansed.model.StationCleansedData;
+import com.mdms.mdms_station.stationuncleansed.model.StationUncleansedData;
 import com.mdms.mdms_station.stationuncleansed.repository.StationCleansedDataRepository;
 @Service
 public class StationAddService {
@@ -29,6 +35,60 @@ StationCleansedDataRepository stn_clnsd_repo;
 		return returnstmt;
 		}
 	
+
+	 @Transactional(rollbackOn = Exception.class)
+	 public String updateCmiDraft(StationCleansedData stationdraftcmi)throws Exception {				
+		String returnstmt="";	
+		try {
+					String cmi_station_code=stationdraftcmi.getStn_Id().getStation_code();
+					Date cmi_valid_from=stationdraftcmi.getStn_Id().getStation_valid_from();
+					Date cmi_valid_upto=stationdraftcmi.getStn_Id().getStation_valid_upto();
+					String userid=stationdraftcmi.getUser_id_cmi();
+					  Date date = new Date();  
+				   stn_clnsd_repo.updateCleansedDraftCmi( userid,  cmi_station_code ,  cmi_valid_from ,  cmi_valid_upto , stationdraftcmi.getStation_numeric_code(),
+						stationdraftcmi.getStation_name(),	stationdraftcmi.getTraffic_type(),stationdraftcmi.getTranshipment_flag() ,
+						stationdraftcmi.getStation_class() ,stationdraftcmi.getJunction_flag(), stationdraftcmi.getInterchange_flag() , 
+						stationdraftcmi.getState(), stationdraftcmi.getPincode() , stationdraftcmi.getDistrict(),stationdraftcmi.getTehsil(), 
+						stationdraftcmi.getStation_short_name(), stationdraftcmi.getInterlocking_standard() , stationdraftcmi.getWorking_division(),
+						stationdraftcmi.getWeight_bridge(), stationdraftcmi.getSiding() , stationdraftcmi.getBooking_type() , stationdraftcmi.getCmi_status(), date,stationdraftcmi.getBooking_resource() );
+				returnstmt="DRAFT UPDATED SUCCESSFULLY";	
+		}
+		catch (Exception e)
+		{
+			System.out.printf(e.getMessage(),e.getStackTrace());
+			returnstmt="FAILED TO UPDATE";	
+		}
+	return returnstmt;
+		}
 	
+	
+	
+
+public String saveCmiCleansedDraft(StationCleansedData savedraftcmi)throws Exception {			
+	String returnstmt="";		
+
+	 
+	    Date date = new Date();  
+		try 	{	
+			savedraftcmi.setTxn_date_cmi(date);
+	
+			System.out.println(savedraftcmi);
+	if(stn_clnsd_repo.save(savedraftcmi)!=null)
+	{
+		returnstmt="DRAFT SAVED SUCCESSFULLY";
+	}
+		else
+	{
+			returnstmt="FAILED TO SAVE DRAFT";
+	}}
+			catch(Exception e)
+			{
+				System.out.printf(e.getMessage(),e.getStackTrace());
+				return "EXCEPTION OCCURRED!!!";
+			}
+
+return returnstmt;
 	
 }
+}
+

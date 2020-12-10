0f9ccb03948b3f250e63e659245b99bb3ac6cea2
anshul <anshul@cris.org.in>
2020-12-10 11:08:46 +0530

    Anshul 10-12-2020

--- src/main/java/com/mdms/mdms_station/stationuncleansed/service/StationAddService.java
+++ src/main/java/com/mdms/mdms_station/stationuncleansed/service/StationAddService.java
@@ -35,76 +35,60 @@ StationCleansedDataRepository stn_clnsd_repo;
 		return returnstmt;
 		}
 	
-public String saveCmiCleansedDraft(StationCleansedData stationdraftcmi)throws Exception {
-	
-	boolean ispresent;
-	String returnstmt="";
-	try {
-	ispresent=stn_clnsd_repo.findById(stationdraftcmi.getStn_Id()).isPresent();//Check if record is already present	
-	if(ispresent)
-	{
-//1.that record is already saved as draft 
-		
-		String status=stn_clnsd_repo.findById(stationdraftcmi.getStn_Id()).get().getCmi_status();
-		String useridcmi=stn_clnsd_repo.findById(stationdraftcmi.getStn_Id()).get().getUser_id_cmi();		
-		if(status.equals("D"))
-			
-		{//by the same user.
-			if(useridcmi.equals(stationdraftcmi.getUser_id_cmi()))
-			{
-				//if this is the same user
-				String cmi_station_code=stationdraftcmi.getStn_Id().getStation_code();
-				Date cmi_valid_from=stationdraftcmi.getStn_Id().getStation_valid_from();
-				Date cmi_valid_upto=stationdraftcmi.getStn_Id().getStation_valid_upto();
-				String userid=stationdraftcmi.getUser_id_cmi();
-				  Date date = new Date();  
-				  stn_clnsd_repo.updateCleansedDraftCmi( userid,  cmi_station_code ,  cmi_valid_from ,  cmi_valid_upto , stationdraftcmi.getStation_numeric_code(),
-					stationdraftcmi.getStation_name(),	stationdraftcmi.getTraffic_type(),stationdraftcmi.getTranshipment_flag() ,
-					stationdraftcmi.getStation_class() ,stationdraftcmi.getJunction_flag(), stationdraftcmi.getInterchange_flag() , 
-					stationdraftcmi.getState(), stationdraftcmi.getPincode() , stationdraftcmi.getDistrict(),stationdraftcmi.getTehsil(), 
-					stationdraftcmi.getStation_short_name(), stationdraftcmi.getInterlocking_standard() , stationdraftcmi.getWorking_division(),
-					stationdraftcmi.getWeight_bridge(), stationdraftcmi.getSiding() , stationdraftcmi.getBooking_type() , stationdraftcmi.getCmi_status(), date ,stationdraftcmi.getBooking_resource()  );
-			returnstmt="DRAFT UPDATED SUCCESSFULLY";	
-			
-			}
-			
-			//2.that record is already saved as draft by the OTHER user.
-			else
-				returnstmt="TRANSACTION ABORTED . THIS DRAFT IS ALREADY SAVED BY OTHER USER.";
-			
+
+	 @Transactional(rollbackOn = Exception.class)
+	 public String updateCmiDraft(StationCleansedData stationdraftcmi)throws Exception {				
+		String returnstmt="";	
+		try {
+					String cmi_station_code=stationdraftcmi.getStn_Id().getStation_code();
+					Date cmi_valid_from=stationdraftcmi.getStn_Id().getStation_valid_from();
+					Date cmi_valid_upto=stationdraftcmi.getStn_Id().getStation_valid_upto();
+					String userid=stationdraftcmi.getUser_id_cmi();
+					  Date date = new Date();  
+				   stn_clnsd_repo.updateCleansedDraftCmi( userid,  cmi_station_code ,  cmi_valid_from ,  cmi_valid_upto , stationdraftcmi.getStation_numeric_code(),
+						stationdraftcmi.getStation_name(),	stationdraftcmi.getTraffic_type(),stationdraftcmi.getTranshipment_flag() ,
+						stationdraftcmi.getStation_class() ,stationdraftcmi.getJunction_flag(), stationdraftcmi.getInterchange_flag() , 
+						stationdraftcmi.getState(), stationdraftcmi.getPincode() , stationdraftcmi.getDistrict(),stationdraftcmi.getTehsil(), 
+						stationdraftcmi.getStation_short_name(), stationdraftcmi.getInterlocking_standard() , stationdraftcmi.getWorking_division(),
+						stationdraftcmi.getWeight_bridge(), stationdraftcmi.getSiding() , stationdraftcmi.getBooking_type() , stationdraftcmi.getCmi_status(), date,stationdraftcmi.getBooking_resource() );
+				returnstmt="DRAFT UPDATED SUCCESSFULLY";	
 		}
-		
+		catch (Exception e)
+		{
+			System.out.printf(e.getMessage(),e.getStackTrace());
+			returnstmt="FAILED TO UPDATE";	
+		}
+	return returnstmt;
+		}
 	
-		
-	else returnstmt="INVALID TRANSACTION";	
-	}
 	
-	else
+	
+
+public String saveCmiCleansedDraft(StationCleansedData savedraftcmi)throws Exception {			
+	String returnstmt="";		
+
+	 
+	    Date date = new Date();  
+		try 	{	
+			savedraftcmi.setTxn_date_cmi(date);
+	
+			
+	if(stn_clnsd_repo.save(savedraftcmi)!=null)
 	{
-//		 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
-		    Date date = new Date();  
-		  
-		stationdraftcmi.setTxn_date_cmi(date);
-		
-		if(stn_clnsd_repo.save(stationdraftcmi)!=null) {
-		
 		returnstmt="DRAFT SAVED SUCCESSFULLY";
-		
 	}
 		else
-		{
-				returnstmt="FAILED TO SAVE DRAFT";
-		}}}
-				catch(Exception e)
-				{
-					System.out.printf(e.getMessage(),e.getStackTrace());
-					return "EXCEPTION OCCURRED!!!";
-				}
+	{
+			returnstmt="FAILED TO SAVE DRAFT";
+	}}
+			catch(Exception e)
+			{
+				System.out.printf(e.getMessage(),e.getStackTrace());
+				return "EXCEPTION OCCURRED!!!";
+			}
 
-	return returnstmt;
-		
-
- 
+return returnstmt;
+	
 }
 }
 

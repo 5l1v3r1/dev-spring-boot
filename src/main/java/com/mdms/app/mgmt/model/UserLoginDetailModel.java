97981bd87f0eac1a1016160f7079bbb4f13b1fda
ritu <ir.mdms.cris.2018@gmail.com>
2021-03-03 17:27:34 +0530

    dashboard report

--- src/main/java/com/mdms/app/mgmt/model/UserLoginDetailModel.java
+++ src/main/java/com/mdms/app/mgmt/model/UserLoginDetailModel.java
@@ -40,6 +40,23 @@ public class UserLoginDetailModel {
 
 	
 	
+	@Column(name="is_active")
+	private String is_active;
+	
+
+	
+
+
+
+
+	public String getIs_active() {
+		return is_active;
+	}
+
+
+	public void setIs_active(String is_active) {
+		this.is_active = is_active;
+	}
 
 	
 

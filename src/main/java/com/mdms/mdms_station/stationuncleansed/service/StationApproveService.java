c3c7a5086127dbf66b13fbb0c16d0b4f54ba1c1e
ritu <ir.mdms.cris.2018@gmail.com>
2021-04-10 12:45:02 +0530

    loco hyper

--- src/main/java/com/mdms/mdms_station/stationuncleansed/service/StationApproveService.java
+++ src/main/java/com/mdms/mdms_station/stationuncleansed/service/StationApproveService.java
@@ -1,5 +1,23 @@
 package com.mdms.mdms_station.stationuncleansed.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mdms.mdms_station.stationuncleansed.model.StationCleansedData;
+import com.mdms.mdms_station.stationuncleansed.model.StationTableRbs;
+import com.mdms.mdms_station.stationuncleansed.repository.StationCleansedDataRepository;
+@Service
 public class StationApproveService {
+	
+@Autowired	
+	StationCleansedDataRepository stn_clnsd_repo;
+	
+public StationCleansedData getTotalCleansedStationHyperDivisionWise(StationCleansedData division_code) throws Exception
+{
+	String divcode=division_code.getDivision_code();
+	return stn_clnsd_repo.getTotalCleansedStationHyperDivisionWise(divcode);
+}
+ 
+
 
 }
